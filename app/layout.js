855b5ab4afcd6a4adccef60203062d2c404fc912
codayomide <smokayomide@gmail.com>
2024-03-21 15:13:59 +0100

    Worked on top message
    Created and styled top message components

--- app/layout.js
+++ app/layout.js
@@ -11,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className="w-full min-h-full">
         <header>
           <TopMsg />
           <NavMenu />
