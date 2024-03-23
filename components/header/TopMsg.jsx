e1d3184bb871b6ed07fb2b3a4cebd65c7696b277
codayomide <smokayomide@gmail.com>
2024-03-23 13:59:30 +0100

    minor style change

--- components/header/TopMsg.jsx
+++ components/header/TopMsg.jsx
@@ -1,6 +1,6 @@
 const TopMsg = () => {
   return (
-    <div className="w-full bg-green text-white border-b-[1.5px] border-b-darkBlue py-[0.5rem] px-[1.5rem] mo-sm:px-[2rem]  text-lg text-center font-medium">Recycled and biodegradable - Handmade in Italy</div>
+    <div className="w-full bg-green text-white border-b-[1.5px] border-b-darkBlue py-[0.5rem] px-[0.5rem] mo-sm:px-[2rem] text-xs md:text-lg text-center font-medium">Recycled and biodegradable - Handmade in Italy</div>
   );
 }
  
