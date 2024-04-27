42592b1f14d690d8a3fe856bb95a02d58b24d6d9
codayomide <smokayomide@gmail.com>
2024-04-27 17:56:42 +0100

    Created structure for bestsellers section

--- components/home/BestSellersSection.jsx
+++ components/home/BestSellersSection.jsx
@@ -1,7 +1,33 @@
+import Image from "next/image";
+
 const BestSellersSection = () => {
   return (
-    <section></section>
+    <section>
+      <h2>Bestsellers</h2>
+      <div>
+        <div>
+          <Image src={`/`} alt="Glasses" width={30} height={30} />
+          <p>Sunglasses</p>
+          <h3>£333</h3>
+        </div>
+        <div>
+          <Image src={`/`} alt="Glasses" width={30} height={30} />
+          <p>Sunglasses</p>
+          <h3>£333</h3>
+        </div>
+        <div>
+          <Image src={`/`} alt="Glasses" width={30} height={30} />
+          <p>Sunglasses</p>
+          <h3>£333</h3>
+        </div>
+        <div>
+          <Image src={`/`} alt="Glasses" width={30} height={30} />
+          <p>Sunglasses</p>
+          <h3>£333</h3>
+        </div>
+      </div>
+    </section>
   );
-}
- 
+};
+
 export default BestSellersSection;
