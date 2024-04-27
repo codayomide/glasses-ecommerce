b5a2be006f47eaecc97a9195df1c595b16e40a63
codayomide <smokayomide@gmail.com>
2024-04-27 17:22:30 +0100

    Created Structure

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,11 @@
+import AboutUs from "@/components/home/AboutUs";
 import HeroSection from "@/components/home/HeroSection";
 
 export default function Home() {
   return (
     <div>
       <HeroSection />
+      <AboutUs />
     </div>
   )
 }
---
+++ components/home/AboutUs.jsx
@@ -0,0 +1,19 @@
+const AboutUs = () => {
+  return (
+    <section>
+      <p>
+        We are Glasses. We love this planet. We also love looking good and we
+        don’t see why anyone should ever have to choose between the two.
+      </p>
+
+      <p>
+        That is why we only use biodegradable and recycled materials to make
+        high quality and stylish eyewear. For you and for your planet.
+      </p>
+
+      <p>So don’t just look good. Look good for your planet!</p>
+    </section>
+  );
+};
+
+export default AboutUs;
