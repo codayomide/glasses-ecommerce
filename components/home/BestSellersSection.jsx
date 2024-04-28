16c9926af1187b76310aebd81e2ec912e8b70889
codayomide <smokayomide@gmail.com>
2024-04-28 21:53:29 +0100

    Working on mobile layout

--- components/home/BestSellersSection.jsx
+++ components/home/BestSellersSection.jsx
@@ -1,28 +1,30 @@
 import Image from "next/image";
 import Link from "next/link";
+import glassesImg from "@/images/glasses/glasses1.webp"
 
 const BestSellersSection = () => {
   return (
-    <section>
-      <h2>Bestsellers</h2>
-      <div>
-        <div>
-          <Image src={`/`} alt="Glasses" width={30} height={30} />
+    <section className="pt-16">
+      <h2 className="text-3xl font-semibold px-8 pb-10">Bestsellers</h2>
+
+      <div className="grid grid-flow-col gap-6 overflow-x-scroll w-full px-8 pb-4">
+        <div className="w-[300px]">
+          <Image src={glassesImg} alt="Glasses" width={300} height={300} />
           <p>Sunglasses</p>
           <h3>£333</h3>
         </div>
-        <div>
-          <Image src={`/`} alt="Glasses" width={30} height={30} />
+        <div className="w-[300px]">
+          <Image src={glassesImg} alt="Glasses" width={300} height={300} />
           <p>Sunglasses</p>
           <h3>£333</h3>
         </div>
-        <div>
-          <Image src={`/`} alt="Glasses" width={30} height={30} />
+        <div className="w-[300px]">
+          <Image src={glassesImg} alt="Glasses" width={300} height={300} />
           <p>Sunglasses</p>
           <h3>£333</h3>
         </div>
-        <div>
-          <Image src={`/`} alt="Glasses" width={30} height={30} />
+        <div className="w-[300px]">
+          <Image src={glassesImg} alt="Glasses" width={300} height={300} />
           <p>Sunglasses</p>
           <h3>£333</h3>
         </div>
