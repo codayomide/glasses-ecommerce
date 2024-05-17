34dfc9299ebccafbaf71315e67da9499b02324f9
codayomide <smokayomide@gmail.com>
2024-05-18 00:15:30 +0100

    Working on footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -20,14 +20,14 @@ const Footer = () => {
         </div>
       </div>
 
-      <div className="border-t-2 px-4 mo-sm:px-8 md:px-14 flex flex-col">
-        <h1>Subscribe to our mailing list</h1>
+      <div className="border-y-[1.5px] px-4 mo-sm:px-8 md:px-14 pt-12 pb-8 flex flex-col">
+        <h1 className="text-xl font-medium mb-4">Subscribe to our mailing list</h1>
         <form className="flex justify-between">
-          <input type="text" />
-          <button>Subscribe</button>
+          <input type="text" placeholder="Your email address" className="p-3 text-black" />
+          <button className="rounded-full border-2 border-white px-6 py-3">Subscribe</button>
         </form>
-        <div className="w-fit self-center">
-          <h1 className="text-center">Follow us</h1>
+        <div className="w-fit self-center mt-6">
+          <h1 className="text-center text-xl">Follow us</h1>
           {/* Add Social Media Icons */}
         </div>
       </div>
